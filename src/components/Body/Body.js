2fd0a43179e5b9a876b475e93625ca4d9442b2e9
Ayush Anand <ayushanand141@gmail.com>
2023-10-22 00:01:53 +0530

    updated web app

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 import ReactToPrint from "react-to-print";
-import { ArrowDown } from "react-feather";
+import { ArrowDown, Palette } from "react-feather";
 
 import Editor from "../Editor/Editor";
 import Resume from "../Resume/Resume";
@@ -60,46 +60,64 @@ function Body() {
   });
 
   return (
+    
     <div className={styles.container}>
-      <p className={styles.heading}>Resume Builder</p>
       <div className={styles.toolbar}>
-        <div className={styles.colors}>
-          {colors.map((item) => (
-            <span
-              key={item}
-              style={{ backgroundColor: item }}
-              className={`${styles.color} ${
-                activeColor === item ? styles.active : ""
-              }`}
-              onClick={() => setActiveColor(item)}
-            />
-          ))}
-        </div>
-        <ReactToPrint
-          trigger={() => {
-            return (
-              <button>
-                Download <ArrowDown />
-              </button>
-            );
-          }}
-          content={() => resumeRef.current}
-        />
-      </div>
-      <div className={styles.main}>
-        <Editor
-          sections={sections}
-          information={resumeInformation}
-          setInformation={setResumeInformation}
-        />
-        <Resume
-          ref={resumeRef}
-          sections={sections}
-          information={resumeInformation}
-          activeColor={activeColor}
-        />
-      </div>
-    </div>
+  <div className={styles.colors}>
+    {colors.map((item) => (
+      <span
+        key={item}
+        style={{ backgroundColor: item }}
+        className={`${styles.color} ${
+          activeColor === item ? styles.active : ""
+        }`}
+        onClick={() => setActiveColor(item)}
+      />
+    ))}
+  </div>
+  <ReactToPrint
+    trigger={() => {
+      return (
+        <button className={styles.downloadButton}>
+          Download <ArrowDown />
+        </button>
+      );
+    }}
+    content={() => resumeRef.current}
+  />
+</div>
+
+<div className={styles.topRow}>
+  <div className={`${styles.editorContainer} ${styles.panel}`}>
+    <Editor
+      sections={sections}
+      information={resumeInformation}
+      setInformation={setResumeInformation}
+    />
+  </div>
+  <div className={`${styles.resumeContainer} ${styles.panel}`}>
+    <Resume
+      ref={resumeRef}
+      sections={sections}
+      information={resumeInformation}
+      activeColor={activeColor}
+    />
+  </div>
+</div>
+
+<div className={styles.instructions}>
+  <h3>Instructions:</h3>
+  <ul>
+    <li>Fill in your professional details in the respective sections.</li>
+    <li>Click the "Save" button to store your input for preview.</li>
+    <li>Customize the theme colors by selecting a preferred color from the palette.</li>
+    <li>Website will automatically preview your resume with the chosen color scheme.</li>
+    <li>Organize the sections by holding, dragging, and shifting them in the order of your choice.</li>
+    <li>Click the "Download" button to generate and save your resume in the selected theme.</li>
+  </ul>
+</div>
+</div>
+
   );
 }
 
